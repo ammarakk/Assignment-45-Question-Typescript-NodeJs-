e8997f0aa8e618a70a63ce4f4968eef6bcb39596
ammarakk <161769688+ammarakk@users.noreply.github.com>
2024-03-13 23:07:31 +0500

    Update

--- 12 Greetings/app.js
+++ 12 Greetings/app.js
@@ -1,4 +1,4 @@
-var Friendsname = ["ali", "abbas", "ammar"];
+var Friendsname = ["Ali", "Abbas", "Ammar"];
 for (var _i = 0, Friendsname_1 = Friendsname; _i < Friendsname_1.length; _i++) {
     var name_1 = Friendsname_1[_i];
     console.log("hello ".concat(name_1, " \n how r u?."));
