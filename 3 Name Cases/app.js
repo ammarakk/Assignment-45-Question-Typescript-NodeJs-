d530f3c4523761b01b1cfd7b5c52ea5c77a3c48f
ammarakk <161769688+ammarakk@users.noreply.github.com>
2024-03-13 22:34:40 +0500

    Delete 3 Name Cases directory

--- 3 Name Cases/app.js
+++
@@ -1,4 +0,0 @@
-var myname = "ahmed";
-console.log(myname.toLowerCase());
-console.log(myname.toUpperCase());
-console.log(myname.charAt(0).toUpperCase() + myname.slice(1).toLowerCase());
--- 3 Name Cases/app.ts
+++
@@ -1,4 +0,0 @@
-let myname:string= "ahmed";
-console.log(myname.toLowerCase());
-console.log(myname.toUpperCase());
-console.log(myname.charAt(0).toUpperCase()+myname.slice(1).toLowerCase());
