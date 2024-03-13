ef676171b5932be72635991048db5c6e2368e5e8
ammarakk <161769688+ammarakk@users.noreply.github.com>
2024-03-13 23:11:38 +0500

    update

---
+++ 14 Guest List/app.js
@@ -0,0 +1,4 @@
+var Guestname = [" TAHA AMIR", "IMRAN", "KHURRAM"];
+Guestname.forEach(function (Guestname) {
+    console.log("Dear ".concat(Guestname, ", would you like to join me for dinner?"));
+});
