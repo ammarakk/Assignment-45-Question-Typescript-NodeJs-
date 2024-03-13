25292d02e925ce89c8acc65e6980f9a638622d31
ammarakk <161769688+ammarakk@users.noreply.github.com>
2024-03-13 23:36:20 +0500

    update

---
+++ 20 Objects/app.js
@@ -0,0 +1,6 @@
+var book = {
+    title: "MY BOOK",
+    author: "AMMAR AHMED KHAN",
+    yearPublished: 2024
+};
+console.log("Book Info: ".concat(book.title, " \nBy ").concat(book.author, ",\nPublished in").concat(book.yearPublished));
