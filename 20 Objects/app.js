efb16ff0b8341af9ad43440447c80d7e258cd3a5
ammarakk <161769688+ammarakk@users.noreply.github.com>
2024-03-13 23:41:41 +0500

    Delete 20 Objects directory

--- 20 Objects/app.js
+++
@@ -1,6 +0,0 @@
-var book = {
-    title: "MY BOOK",
-    author: "AMMAR AHMED KHAN",
-    yearPublished: 2024
-};
-console.log("Book Info: ".concat(book.title, " \nBy ").concat(book.author, ",\nPublished in").concat(book.yearPublished));
