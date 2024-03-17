95490aa7b168359f133546aeab6075edbfb8104c
ammarakk <161769688+ammarakk@users.noreply.github.com>
2024-03-17 22:43:24 +0500

    update

---
+++ 31 No User/app.js
@@ -0,0 +1,9 @@
+var username = [];
+if (username.length === 0) {
+    console.log("We need to find some users!");
+}
+else {
+    // Greet users
+}
+// Removing all usernames ensures the message "We need to find some users!" is -
+// -printed.
