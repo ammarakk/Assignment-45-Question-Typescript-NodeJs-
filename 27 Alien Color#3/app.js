1024b6f62fa34feb892530ab977448542fc28c06
ammarakk <161769688+ammarakk@users.noreply.github.com>
2024-03-17 22:26:01 +0500

    update

---
+++ 27 Alien Color#3/app.js
@@ -0,0 +1,33 @@
+console.log("green alien version ");
+var alien_color3 = "green";
+if (alien_color3 == "green") {
+    console.log("You earned 5 points.");
+}
+else if (alien_color3 == "yellow") {
+    console.log("You earned 10 points.");
+}
+else if (alien_color3 == "red") {
+    console.log("You earned 15 points.");
+}
+console.log("Yellow alien version:");
+alien_color3 = "yellow";
+if (alien_color3 == "green") {
+    console.log("You earned 5 points.");
+}
+else if (alien_color3 == "yellow") {
+    console.log("You earned 10 points.");
+}
+else if (alien_color3 == "red") {
+    console.log("You earned 15 points.");
+}
+console.log("Red alien version:");
+alien_color3 = "red";
+if (alien_color3 == "green") {
+    console.log("You earned 5 points.");
+}
+else if (alien_color3 == "yellow") {
+    console.log("You earned 10 points.");
+}
+else if (alien_color3 == "red") {
+    console.log("You earned 15 points.");
+}
