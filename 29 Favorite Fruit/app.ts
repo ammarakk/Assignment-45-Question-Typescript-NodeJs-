1df0630f1f91b02c7cad31a3adf52a7ed9106086
ammarakk <161769688+ammarakk@users.noreply.github.com>
2024-03-17 22:37:30 +0500

    update

---
+++ 29 Favorite Fruit/app.ts
@@ -0,0 +1,15 @@
+let favorite_fruits: string[] = ["apples", "bananas", "cherries","orange"];
+
+if (favorite_fruits.includes("bananas")) {
+    console.log("You really like bananas!");
+}
+if (favorite_fruits.includes("apples")) {
+    console.log("You really like apples!");
+}
+// Continue with more fruits
+if (favorite_fruits.includes("cherries")) {
+    console.log("You really like cherries!");
+}
+if (favorite_fruits.includes("orange")) {
+    console.log("You really like orange!");
+}    
