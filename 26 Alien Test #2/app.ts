218ddab45a55bb53386278ca8fa773e015b9f9a3
ammarakk <161769688+ammarakk@users.noreply.github.com>
2024-03-17 22:19:01 +0500

    update

---
+++ 26 Alien Test #2/app.ts
@@ -0,0 +1,15 @@
+let alien_color2 ="green";
+if (alien_color2 == "green") {
+    console.log("You just earned 20 points for shooting the alien!");
+} else {
+    console.log("You just earned 15 points.");
+}
+
+console.log("Version that runs the else block:");
+
+alien_color2 = "yellow";
+if (alien_color2 == "green") {
+    console.log("You just earned 5 points for shooting the alien!");
+} else {
+    console.log("choose yellow You just earned 10 points.");
+}
