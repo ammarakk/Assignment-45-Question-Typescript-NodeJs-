9b1d4216231b5f6f6ed0f6a6d49966fb2051db6b
ammarakk <161769688+ammarakk@users.noreply.github.com>
2024-03-17 21:48:15 +0500

    update

---
+++ 21 Object/app.ts
@@ -0,0 +1,8 @@
+let book: { title: string; author: string; yearPublished: number } = {
+    title: "MY BOOK",
+    author: "AMMAR AHMED KHAN",
+    yearPublished: 2024
+};
+console.log(`Book Info: ${book.title} 
+By : ${book.author},
+Published in :${book.yearPublished}`);
