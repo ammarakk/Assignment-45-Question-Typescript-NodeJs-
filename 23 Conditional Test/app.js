f2ee59097a577945580b597bc46b8ec6d49eb443
ammarakk <161769688+ammarakk@users.noreply.github.com>
2024-03-17 22:04:41 +0500

    update

---
+++ 23 Conditional Test/app.js
@@ -0,0 +1,5 @@
+var bike_name = 'honda';
+console.log("Is bike_name == 'honda'? I predict True.");
+console.log(bike_name == 'honda'); // True
+console.log("Is bike_name == 'toyota'? I predict False.");
+console.log(bike_name == 'toyota'); // False
