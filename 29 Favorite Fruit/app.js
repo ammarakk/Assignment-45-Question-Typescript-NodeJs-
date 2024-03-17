fb73696912039879940f11223111a16c7ec884ae
ammarakk <161769688+ammarakk@users.noreply.github.com>
2024-03-17 22:36:26 +0500

    update

---
+++ 29 Favorite Fruit/app.js
@@ -0,0 +1,14 @@
+var favorite_fruits = ["apples", "bananas", "cherries", "orange"];
+if (favorite_fruits.includes("bananas")) {
+    console.log("You really like bananas!");
+}
+if (favorite_fruits.includes("apples")) {
+    console.log("You really like apples!");
+}
+// Continue with more fruits
+if (favorite_fruits.includes("cherries")) {
+    console.log("You really like cherries!");
+}
+if (favorite_fruits.includes("orange")) {
+    console.log("You really like orange!");
+}
