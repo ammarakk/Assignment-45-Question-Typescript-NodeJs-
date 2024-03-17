280dc3af9bda4722d20cdbac8c2b2ee1072ddaa4
ammarakk <161769688+ammarakk@users.noreply.github.com>
2024-03-17 21:50:25 +0500

    update

---
+++ 22  International Error/app.js
@@ -0,0 +1,5 @@
+var friendsname = ["RIAZ", "HAMEED", "ASIF", "ARSHAD"];
+console.log(friendsname[4]);
+// Intentional error: Arrays are zero-indexed, so index 4 is out of bounds.
+//friendsname[3] = "ARSHAD"; Correcting the error by accessing a valid index.
+console.log(friendsname[3]);
