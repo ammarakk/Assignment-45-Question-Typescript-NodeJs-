a8bb15360501b0da3de307f44d43d4045f7d9a64
ammarakk <161769688+ammarakk@users.noreply.github.com>
2024-03-17 23:59:15 +0500

    update

---
+++ 41 Magicians/app.js
@@ -0,0 +1,7 @@
+var magicians = ["JOJO", "BOBBY", "VICKY"];
+function show_magicians(magicians) {
+    magicians.forEach(function (magician) {
+        console.log(magician);
+    });
+}
+show_magicians(magicians);
