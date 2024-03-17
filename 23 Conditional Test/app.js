f7f6e55f1cd1414feae2945f674d8c9a8ec296e1
ammarakk <161769688+ammarakk@users.noreply.github.com>
2024-03-17 21:58:21 +0500

    Update

--- 23 Conditional Test/app.js
+++ 23 Conditional Test/app.js
@@ -1,5 +1,25 @@
-var bike_name = 'honda';
-console.log("Is bike_name == 'honda'? I predict True.");
-console.log(bike_name == 'honda'); // True
-console.log("Is bike_name == 'toyota'? I predict False.");
-console.log(bike_name == 'toyota'); // False
+var fruit = "apple";
+console.log("Testing equality with strings:");
+console.log(fruit == "apple"); // True
+console.log(fruit == "Apple"); // False
+// Using the lower case function
+console.log("Testing with lower case:");
+console.log("Apple".toLowerCase() == "apple"); // True
+// Numerical tests
+var num = 6;
+console.log("Numerical tests:");
+console.log(num > 5); // True
+console.log(num < 1); // False
+// Tests using "and" and "or" operators
+var Tests = ["true" && "false"];
+var tests = ["true" || "false"];
+console.log("Tests with 'and' and 'or':");
+console.log(true && false); // False
+console.log(true || false); // True
+// Test whether an item is in a array
+var fruits = ["apple", "banana", "cherry"];
+console.log("Is 'apple' in fruits?");
+console.log(fruits.includes("apple")); // True
+// Test whether an item is not in a array
+console.log("Is 'mango' not in fruits?");
+console.log(!fruits.includes("mango")); // True
